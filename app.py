1575d661f2954ee3c75990f700aacec810f7d39e
Thaweewat <104571571+RThaweewat@users.noreply.github.com>
2023-06-03 22:41:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,26 @@
 import streamlit as st
-from scipy.stats import norm
 
 # Define data
 population = 71.6 * 10**6  # Total population
 
-age_brackets = {
-    '14-24': 0.1405,
-    '25-54': 0.4497,
-    '55-64': 0.1344,
-    '65+': 0.1307,
+age_distribution = {
+    'men': {
+        18: 0.0132, 19: 0.0128, 20: 0.0128, 21: 0.0129, 22: 0.0135, 23: 0.0135, 24: 0.0144,
+        25: 0.0156, 26: 0.0159, 27: 0.0157, 28: 0.0151, 29: 0.0152, 30: 0.0154, 31: 0.0153,
+        32: 0.015, 33: 0.0144, 34: 0.0141, 35: 0.014, 36: 0.0144, 37: 0.0149, 38: 0.015,
+        39: 0.0153, 40: 0.0158, 41: 0.0158, 42: 0.0161, 43: 0.016, 44: 0.0154, 45: 0.0161,
+        46: 0.0158, 47: 0.0155, 48: 0.0157, 49: 0.0153, 50: 0.0156
+    },
+    'women': {
+        18: 0.0119, 19: 0.0116, 20: 0.0117, 21: 0.0117, 22: 0.0124, 23: 0.0123, 24: 0.0132,
+        25: 0.0143, 26: 0.0146, 27: 0.0144, 28: 0.0139, 29: 0.0141, 30: 0.0143, 31: 0.0142,
+        32: 0.014, 33: 0.0135, 34: 0.0132, 35: 0.0132, 36: 0.0137, 37: 0.0142, 38: 0.0143,
+        39: 0.0147, 40: 0.0152, 41: 0.0153, 42: 0.0156, 43: 0.0156, 44: 0.0152, 45: 0.016,
+        46: 0.0158, 47: 0.0155, 48: 0.0159, 49: 0.0156, 50: 0.0161
+    },
 }
-education_levels = {
-    'Below Bachelor': 0.305,
-    'Bachelor': 0.634,
-    'Above Bachelor': 0.07,
-}
-height_dist = {
-    'women': 160,
-    'men': 170,
-}
-height_std = 10
-income_brackets = {
-    '<175k': 0.19,
-    '175k-350k': 0.40,
-    '350k-525k': 0.22,
-    '525k-875k': 0.13,
-    '>875k': 0.06,
-}
-exercise_ratio = 0.261
-fat_ratio = 0.472
-car_ratio = 0.227
-smoke_ratio = 0.191
-drink_ratio = 0.28
-pet_ratio = 0.49
+
+# other data remain the same ...
 
 # Begin Streamlit
 st.title("Your Dream Partner Probability Calculator")
@@ -42,7 +30,7 @@ col1, col2 = st.columns(2)
 
 with col1:
     # Inputs
-    age = st.slider("Age", 18, 50)
+    age_range = st.slider("Age Range", 18, 50, (20, 30))
     min_height = st.slider("Min Height", 140, 240)
     gender = st.radio("Your partner gender", ['men', 'women'])
     car = st.radio("Have private car or not", ['Yes', 'No', 'Any'])
@@ -59,7 +47,7 @@ with col2:
 # Button
 if st.button('Calculate'):
     # Calculate based on inputs
-    age_bracket = next((value for key, value in age_brackets.items() if int(key.split('-')[0]) <= age <= int(key.split('-')[-1])), 0)
+    age_prob = sum(v for k, v in age_distribution[gender].items() if age_range[0] <= k <= age_range[1])
     education_level = education_levels[education]
     height_mean = height_dist[gender]
     income_bracket = income_brackets[income]
