bc0471e921a9080f6360e94ceab91cb83405e2c9
Thaweewat <104571571+RThaweewat@users.noreply.github.com>
2023-06-03 20:24:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,71 @@
 import streamlit as st
 import numpy as np
-import pandas as pd
+import scipy.stats as stats
 
-# Define the statistical data
-age_data = {'14 years or younger': 0.1605, '15-24 years': 0.1248, '25-54 years': 0.4497, '55-64 years': 0.1344, '65 years or older': 0.1307}
-education_data = {'Below Bachelor Degree': 0.305, 'Bachelor Degree': 0.634, 'More than Bachelor Degree': 0.07}
-height_data = {'Mean Women Height': 160, 'Mean Men Height': 170}
-income_data = {'Less than 175,000 THB': 0.19, '175,001 - 350,000 THB': 0.4, '350,001 - 525,000 THB': 0.22, '525,001 - 875,000 THB': 0.13, 'More than 875,000 THB': 0.06}
-exercise_data = {'Exercise': 0.261, 'No Exercise': 0.739}
-fat_data = {'Fat': 0.472, 'Not Fat': 0.528}
 
-# Define the Streamlit app
-def app():
-    st.title('Dream Partner Probability Calculator')
-    st.write('Please select the following options to calculate the probability of finding your dream partner.')
-    
-    # Age selection
-    age = st.selectbox('Select your age range:', list(age_data.keys()))
-    
-    # Education selection
-    education = st.selectbox('Select your education level:', list(education_data.keys()))
-    
-    # Height selection
-    height = st.slider('Select your height (cm):', min_value=140, max_value=200, step=1)
-    
-    # Income selection
-    income = st.selectbox('Select your annual income (THB):', list(income_data.keys()))
-    
-    # Exercise selection
-    exercise = st.radio('Do you exercise regularly?', list(exercise_data.keys()))
-    
-    # Fat selection
-    fat = st.radio('Are you overweight?', list(fat_data.keys()))
-    
-    # Calculate the probability
-    probability = age_data[age] * education_data[education] * np.exp(-((height-height_data['Mean Men Height'])**2)/(2*10**2)) * income_data[income] * exercise_data[exercise] * fat_data[fat]
-    
-    # Display the result
-    st.write('The probability of finding your dream partner is:', round(probability*100, 2), '%')
+# Define the options
+age_groups = {
+    '14 years': (0.1605, 5454539, 5151825),
+    '15-24 years': (0.1248, 4221749, 4023668),
+    '25-54 years': (0.4497, 14712579, 15005961),
+    '55-64 years': (0.1344, 4136063, 4748248),
+    '65 years and over': (0.1307, 3745685, 4890158)
+}
 
-    if __name__ == '__main__':
-        app()
+education_levels = {
+    'Below bachelor degree': 0.305,
+    'Bachelor degree': 0.634,
+    'More than bachelor degree': 0.07
+}
+
+income_groups = {
+    '< 175,000': 0.19,
+    '175,001 - 350,000': 0.40,
+    '350,001 - 525,000': 0.22,
+    '525,001 - 875,000': 0.13,
+    '> 875,000': 0.06
+}
+
+exercise_habits = {
+    'Exercise': 0.261,
+    'No exercise': 0.739
+}
+
+body_weights = {
+    'Fat': 0.472,
+    'Not fat': 0.528
+}
+
+# Define mean and standard deviation for height
+mean_height_woman = 160
+mean_height_man = 170
+std_dev_height = 5.5  # Assuming standard deviation
+
+st.title('Find Your Dream Partner')
+
+gender = st.selectbox('Gender', ['male', 'female'])
+
+age_group = st.selectbox('Age Group', list(age_groups.keys()))
+education_level = st.selectbox('Education Level', list(education_levels.keys()))
+income_group = st.selectbox('Income Group', list(income_groups.keys()))
+exercise_habit = st.selectbox('Exercise Habit', list(exercise_habits.keys()))
+body_weight = st.selectbox('Body Weight', list(body_weights.keys()))
+
+total_population = sum(val[1] if gender == 'male' else val[2] for val in age_groups.values())
+
+p_age = age_groups[age_group][0]
+p_education = education_levels[education_level]
+p_income = income_groups[income_group]
+p_exercise = exercise_habits[exercise_habit]
+p_body_weight = body_weights[body_weight]
+
+# For height, we use a normal distribution
+if gender == 'male':
+    p_height = stats.norm(mean_height_man, std_dev_height).pdf(height)
+else:
+    p_height = stats.norm(mean_height_woman, std_dev_height).pdf(height)
+
+probability = p_age * p_education * p_income * p_exercise * p_body_weight * p_height
+
+# Display the probability
+st.write('Probability:', probability)
